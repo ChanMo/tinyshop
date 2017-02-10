77f9b68e8ddfca099b748397e7a9f491811cea0e
ChanMo <chen.orange@aliyun.com>
2017-02-10 15:47:19 +0800

    add slide to home, add cart modal

--- docs/js/_slide.js
+++ docs/js/_slide.js
@@ -1,69 +1,136 @@
-var slide_startX;
-var slide_startY;
-var slide_dist;
+/**
+ * Slide
+ * http://github.com/ChanMo/juss.git
+ *
+ * @version 2.0.0
+ * @date 28/12/16 16:31:11
+ * @author chen
+ * @email chen.orange@aliyun.com
+ *
+ * Example useage:
+ * $("#slide").slide({
+ *   auto: true,
+ *   time: 2000
+ * });
+ *
+ */
 
-/** set width **/
-var slide_width = parseInt($(".slide").css("width"));
-console.log(slide_width);
-var slide_count = $(".slide__main li").length;
-$(".slide__main").css({"width":slide_count*slide_width,"margin-left":-1*slide_width});
-$(".slide__main li").css({"width":slide_width});
+// plugin constructor
+var Slide = function(elements, subjects, options){
+  this.elements = elements;
+  this.subjects = subjects;
+  this.width;
+  this.timer;
+  this.options = jQuery.extend({}, this.defaults, options);
+  this.attach();
+};
 
-/** set init **/
-$(".slide__main").prepend($(".slide__main li:last-child"));
+// plugin prototype
+Slide.prototype = {
+  defaults: {
+    auto: 'true',
+    time: 3000
+  },
 
-/** start **/
-$(".slide__main").bind("touchstart", function(e){
-  var obj = e.changedTouches[0];
-  slide_startX = obj.pageX;
-  //slide_startY = obj.pageY;
-  e.preventDefault();
-}).bind("touchmove", function(e){
-  e.preventDefault();
-}).bind("touchend", function(e){
-  var obj = e.changedTouches[0];
-  slide_dist = obj.pageX - slide_startX;
-  //slide_distY = obj.pageY - slide_startY;
-  if(slide_dist > 50){
-    slide_to_right();
-  }else if(slide_dist < -50){
-    slide_to_left();
+  // main
+  attach:function(){
+    var self = this;
+    var width = parseInt($(self.elements).css("width"));
+    this.width = width;
+    var count = $(self.elements).find("li").length;
+    var list = $(self.elements).find(".slide__main");
+    var nav = $(self.elements).find(".slide__nav");
+
+    /** set css **/
+    list.css({"width":count*width,"margin-left":-1*width});
+    list.find("li").css({"width":width});
+    list.prepend(list.find("li:last-child"));
+
+    self.bindAction();
+    self.bindSwipe();
+    self.slideAuto();
+  },
+
+  // bind swipe
+  bindSwipe: function(){
+    var self = this;
+    var list = $(self.elements).find(".slide__main");
+		var lastX;
+
+		list.on("touchstart", function(e){
+      lastX = e.changedTouches[0].pageX;
+		});
+		list.on("touchend", function(e){
+      currentX = e.changedTouches[0].pageX;
+      distX = currentX - lastX;
+      if(Math.abs(distX) >= 150){
+        clearInterval(self.timer);
+        if(distX < 0){
+          self.slideToLeft();
+        }else{
+          self.slideToRight();
+        }
+        self.slideAuto();
+      }
+		});
+  },
+
+  // bind action
+  bindAction: function(){
+    var self = this;
+    $(self.elements).find(".slide__action_left").on("click", function(e){
+      e.preventDefault();
+      clearInterval(self.timer);
+      self.slideToRight();
+      self.slideAuto();
+    });
+
+    $(self.elements).find(".slide__action_right").on("click", function(e){
+      e.preventDefault();
+      clearInterval(self.timer);
+      self.slideToLeft();
+      self.slideAuto();
+    });
+  },
+
+
+  // slide auto
+  slideAuto: function(){
+    var self = this;
+    if(self.options['auto']){
+      self.timer = setInterval(function(){
+        self.slideToLeft();
+      }, self.options['time']);
+    }
+  },
+
+  // slide to left
+  slideToLeft: function(){
+    var self = this;
+    var list = $(self.elements).find(".slide__main");
+    var nav = $(self.elements).find(".slide__nav");
+    list.animate({left:-self.width}, 1000, function(){
+      list.append(list.find("li:first-child"));
+      list.css({left:''});
+      nav.prepend(nav.find("li:last-child"));
+    });
+  },
+
+  // slide to right
+  slideToRight: function(){
+    var self = this;
+    var list = $(self.elements).find(".slide__main");
+    var nav = $(self.elements).find(".slide__nav");
+    list.animate({left:+self.width}, 1000, function(){
+      list.prepend(list.find("li:last-child"));
+      list.css({left:''});
+      nav.append(nav.find("li:first-child"));
+    });
   }
-  e.preventDefault();
-});
 
-$(".slide__action_left").bind("click", function(e){
-  e.preventDefault();
-  slide_to_right();
-});
-$(".slide__action_right").bind("click", function(e){
-  e.preventDefault();
-  slide_to_left();
-});
-
-function slide_to_left() {
-  console.log('to_left');
-  var slide = $(".slide__main");
-  var nav = $(".slide__nav");
-  var width = slide.find("li").width();
-  console.log(width);
-  var item = slide.find("li:first-child");
-  slide.animate({left:-width}, 1000, function(){
-    slide.append(item);
-    slide.css({left:''});
-    nav.prepend(nav.find("li:last-child"));
-  });
 }
 
-function slide_to_right(){
-  console.log('to_right');
-  var slide = $(".slide__main");
-  var nav = $(".slide__nav");
-  var width = slide.find("li").width();
-  var item = slide.find("li:last-child");
-  slide.animate({left:+width}, 1000, function(){
-    slide.prepend(item);
-    slide.css({left:''});
-    nav.append(nav.find("li:first-child"));
-  });
-}
+jQuery.fn.slide = function(subjects, options){
+  new Slide(this, jQuery(subjects), options);
+  return this;
+};
